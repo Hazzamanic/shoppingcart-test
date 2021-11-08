b1d8c6460a64fc2ae52ad9b758693234e610cc16
Hazzamanic <breakitup@hotmail.co.uk>
2021-11-08 06:57:31 +0000

    Add some class comments

--- ShoppingBasketTest/Interfaces/IDiscount.cs
+++ ShoppingBasketTest/Interfaces/IDiscount.cs
@@ -5,6 +5,12 @@ namespace ShoppingBasketTest.Interfaces
 {
     public interface IDiscount
     {
+        /// <summary>
+        /// Calculates the discount to apply to a list of items and what items are affected by the discount. A return value indicates whether a discount was applied or not.
+        /// </summary>
+        /// <param name="items"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
         bool TryApplyDiscount(List<ShoppingBasketItem> items, out DiscountResult result);
     }
 }
--- ShoppingBasketTest/Models/DiscountResult.cs
+++ ShoppingBasketTest/Models/DiscountResult.cs
@@ -10,7 +10,13 @@ namespace ShoppingBasketTest.Models
             Items = items;
         }
 
+        /// <summary>
+        /// Items used in the discount
+        /// </summary>
         public IEnumerable<ShoppingBasketItem> Items { get; }
+        /// <summary>
+        /// Discount amount
+        /// </summary>
         public int Amount { get; }
     }
 }
